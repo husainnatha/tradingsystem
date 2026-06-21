03bb117fe40488a286f89e59ed72595148f2893a
Husain <husain.natha@gmail.com>
2026-06-21 17:35:02 +0100

    ammend path definition in tests to be relative

--- tests2/test2_base_dir.py
+++ tests2/test2_base_dir.py
@@ -1,10 +1,13 @@
-from app.config.environment import (
-    BASE_DIR
-)
+from pathlib import Path
+from app.config.environment import BASE_DIR
+import app.config.environment as env
 
-def test_base_dir ():
-
+def test_check_base_dir ():
     print(BASE_DIR)
-
     assert True
+
+def test_base_dir():
+    expected = Path(env.__file__).resolve().parents[2]
+    assert BASE_DIR == expected
+
     
--- tests2/test2_generate_equities_with_metadata.py
+++ tests2/test2_generate_equities_with_metadata.py
@@ -1,9 +1,15 @@
 import yaml
 from pathlib import Path
+from app.config.environment import (
+    BASE_DIR
+)
 
 def test_generate_equities_with_metadata():
-    metadata_path = Path(r"C:\Users\husainnatha\projects\tradingsystem\config\metadata\ticker_metadata.yaml")
-    output_path = Path(r"C:\Users\husainnatha\projects\tradingsystem\config\watchlists\equities_with_metadata.yaml")
+
+    # Paths
+
+    metadata_path = BASE_DIR / "config" / "metadata" / "ticker_metadata.yaml"
+    output_path = BASE_DIR / "config" / "watchlists" / "equities_with_metadata.yaml"
 
     metadata = yaml.safe_load(metadata_path.read_text())
     valid_tickers = list(metadata.keys())
