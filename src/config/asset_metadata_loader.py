33be41c8fbedcd18180a436891e8cdca9524d434
Husain <husain.natha@gmail.com>
2026-06-25 21:29:43 +0100

    initial commit

--- src/config/asset_metadata_loader.py
+++ src/config/asset_metadata_loader.py
@@ -1,29 +1,23 @@
 from pathlib import Path
+
 import yaml
 
-from app.config.environment import (
-    BASE_DIR
-)
 
+class AssetMetadataLoader:
 
-class CoreMacroMetadataLoader:
     @staticmethod
     def load():
-        path = BASE_DIR / "config" / "metadata" / "core_macro_metadata.yaml"
-        with open(path, "r") as file:
-            return yaml.safe_load(file)
 
+        path = (
 
-class TickerLoader:
-    @staticmethod
-    def load():
-        path = BASE_DIR / "config" / "watchlists" / "equities.yaml"
-        with open(path, "r") as file:
-            return yaml.safe_load(file)
+            Path("config")
 
-class TickerMetadataLoader:
-    @staticmethod
-    def load():
-        path = BASE_DIR / "config" / "metadata" / "stock_metadata.yaml"
-        with open(path, "r") as file:
+            / "asset_metadata.yaml"
+        )
+
+        with open(
+            path,
+            "r"
+        ) as file:
+
             return yaml.safe_load(file)
